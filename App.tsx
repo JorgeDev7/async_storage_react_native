4840519de88340b559a7aaf1d17e5e1385ee047a
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-06-04 22:07:54 -0600

    feat: example of AsyncStorage

--- App.tsx
+++ App.tsx
@@ -1,118 +1,115 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
+  TextInput,
+  Button,
+  TouchableHighlight,
+  Alert
 } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const [inputText, setInputText] = useState('');
+  const [isInStorage, setIsInStorage] = useState(false);
+
+  useEffect(() => {
+    const getDataAS = async () => {
+      const nameAS = await AsyncStorage.getItem('name');
+      const hasName: string = nameAS ? JSON.parse(nameAS) : '';
+      if (hasName) {
+        setInputText(hasName);
+        setIsInStorage(true);
+      }
+    };
+
+    getDataAS();
+  }, []);
+
+  const handleInputText = (text: string) => {
+    setInputText(text);
   };
 
+  const safeData = async () => {
+    try {
+      if (!inputText) {
+        Alert.alert('Campo vacío', 'Debes llenar el campo de nombre para continuar');
+        return;
+      }
+      await AsyncStorage.setItem('name', JSON.stringify(inputText));
+      setIsInStorage(true);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteName = async () => {
+    try {
+      await AsyncStorage.removeItem('name');
+      setInputText('');
+      setIsInStorage(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  //todo añadir animación
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      {isInStorage ? <Text>Hola: {inputText}</Text> : ''}
+      <View>
+        <TextInput
+          placeholder='Escribe tu nombre'
+          style={styles.input}
+          value={inputText}
+          onChangeText={value => handleInputText(value)}
+        />
+        <Button
+          title='Guardar'
+          color={'#333'}
+          onPress={() => safeData()}
+        />
+        {isInStorage &&
+          <TouchableHighlight
+            style={styles.deleteButton}
+            onPress={() => deleteName()}
+          >
+            <Text style={styles.deleteText}>Eliminar Nombre</Text>
+          </TouchableHighlight>}
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    alignItems: 'center',
+    justifyContent: 'center'
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  input: {
+    borderColor: "#666",
+    borderBottomWidth: 1,
+    width: 300,
+    height: 40
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  deleteButton: {
+    backgroundColor: '#ff3030',
+    borderRadius: 5,
+    marginTop: 20,
+    padding: 10
   },
-  highlight: {
-    fontWeight: '700',
-  },
+  deleteText: {
+    color: "#FFF",
+    fontWeight: 'bold',
+    textAlign: 'center',
+    textTransform: 'uppercase'
+  }
 });
 
 export default App;
